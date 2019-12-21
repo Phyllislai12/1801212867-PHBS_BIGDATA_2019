3548160b81344ee4bb20a0854b36ca5066c2218a
Phyllislai12 <51715109+Phyllislai12@users.noreply.github.com>
2019-12-21 19:48:35 +0800

    Update Probelm 3.py

--- HW2/Probelm 3.py
+++ HW2/Probelm 3.py
@@ -1,54 +1,91 @@
+#encoding:utf-8
 import numpy as np
 import pandas as pd
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn import linear_model
 
- 
-def vif(X, thres=10.0):
+# [Problem 3.1] remove features with high linearity according to VIF
+# VIF variance inflation factor, which measures the linearity between features 
+def vif(X, thres=1000):#The threshold is set to 1000
     col = list(range(X.shape[1]))
-    dropped = True
+    dropped = True#If drop == True, then all calculated vifs are within the threshold and the traversal stops
     while dropped:
         dropped = False
-        vif = [variance_inflation_factor(X.iloc[:,col].values, ix) for ix in range(X.iloc[:,col].shape[1])]
+        vif = [variance_inflation_factor(X.iloc[:,col].values, ix) for ix in range(X.iloc[:,col].shape[1])]#Calculate the VIF values for eight variables
         maxvif = max(vif)
         maxix = vif.index(maxvif)
         if maxvif > thres:
-            del col[maxix]
             print('delete=',X.columns[col[maxix]],'  ', 'vif=',maxvif )
+            del col[maxix]
             dropped = True
     print('Remain Variables:', list(X.columns[col]))
     print('VIF:', vif)
     return list(X.columns[col]) 
 
-dataset = pd.read_csv("./data/climate_change_1.csv")
-X = dataset.get(["MEI","CO2","CH4","N2O","CFC-11","CFC-12","TSI","Aerosols"])
+'''
+[Problem 3.2]The following program is a model for simple linear regression based on the above results
+'''
+def closed_form_1():
+    #Create a general linear fit with intercept terms
+    dataset = pd.read_csv("./data/climate_change_1.csv")
+    X = dataset.get(['MEI', 'CO2', 'CFC-11', 'CFC-12', 'Aerosols'])#The result of the above selection
+    
+    X = np.column_stack((X,np.ones(len(X))))
+   
+    y = dataset.get("Temp")
 
-y = dataset.get("Temp")
+    X_train = X[:284]
+    X_test = X[284:]
+    y_train = y[:284]
+    y_test = y[284:]
+    
+    X_train=np.mat(X_train)
+    y_train = np.mat(y_train).T
+    xTx = X_train.T*X_train
+    w = 0
+    if np.linalg.det(xTx)==0.0:
+        print("xTx irreversible")
+    else:
+        w = np.ravel(xTx.I*(X_train.T*y_train))
+     
+    coef_=w[:-1]
+    intercept_=w[-1]
+      
+    X_train=X_train[:,0:5]
+    X_test = X_test[:,0:5]
+    d = 0
+    for i in range(5):
+        d += coef_[i]*X_train[:,i]
+    y_train_pred = d+intercept_
+      
+    s = 0
+    for i in range(5):
+        s += coef_[i]*X_test[:,i]
+    y_test_pred = s+intercept_
+      
+      
+    X_train = np.ravel(X_train).reshape(-1,5)
+    y_train = np.ravel(y_train)
+    y_train_pred = np.ravel(y_train_pred)
+      
+    print("Coefficient: ",coef_)
+    print("Intercept: ",intercept_)
+    print("the model is： y = ",coef_,"* X +(",intercept_,")")
+      
+    y_train_avg = np.average(y_train)
+    
+    R2_train = np.sum((y_train_pred-y_train_avg)**2)/(np.sum((y_train-y_train_avg)**2))
+    print("R2 in Train ： ",R2_train)
+      
+    y_test_avg = np.average(y_test)
+    R2_test = np.sum((y_test_pred-y_test_avg)**2)/(np.sum((y_test-y_test_avg)**2))
+    print("R2 in Test ： ",R2_test)
+    
+      
+   
 
-X_train = X[:284]
-X_test = X[284:]
-y_train = y[:284]
-y_test = y[284:]
-d = vif(X_train)
-print(d)
+closed_form_1()
 
-X = dataset.get( ['MEI', 'CFC-12', 'Aerosols'])
-y = dataset.get("Temp")
-X_train = X[:284]
-X_test = X[284:]
-y_train = y[:284]
-y_test = y[284:]
-
-regr = linear_model.LinearRegression()
-regr.fit(X_train,y_train)
-print('coefficients(b1,b2...):',regr.coef_)
-print('intercept(b0):',regr.intercept_)
-y_train_pred = regr.predict(X_train)
-       
-R2_1 = regr.score(X_train, y_train)
-print(R2_1)
-R2_2 = regr.score(X_test, y_test)
-print(R2_2)
 
 
 
